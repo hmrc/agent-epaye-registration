9f9b2f386559d1d8fead2042dd7be77578f73523
Raveen Vijayan <raveen.vijayan@digital.hmrc.gov.uk>
2017-08-04 16:39:38 +0100

    [APB-934][JR,RV] Add validations to input json request

--- app/uk/gov/hmrc/agentepayeregistration/services/AgentEpayeRegistrationService.scala
+++ app/uk/gov/hmrc/agentepayeregistration/services/AgentEpayeRegistrationService.scala
@@ -2,15 +2,25 @@ package uk.gov.hmrc.agentepayeregistration.services
 
 import javax.inject.{Inject, Singleton}
 
-import uk.gov.hmrc.agentepayeregistration.models.{AgentReference, RegistrationRequest}
+import cats.data.Validated.{Invalid, Valid}
+import uk.gov.hmrc.agentepayeregistration.models.{AgentReference, Failure, RegistrationRequest}
 import uk.gov.hmrc.agentepayeregistration.repository.AgentEpayeRegistrationRepository
+import uk.gov.hmrc.agentepayeregistration.validators.AgentEpayeRegistrationValidator
 
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class AgentEpayeRegistrationService @Inject()(repository: AgentEpayeRegistrationRepository) {
+class AgentEpayeRegistrationService @Inject()(
+                                               repository: AgentEpayeRegistrationRepository,
+                                               validator: AgentEpayeRegistrationValidator) {
 
-  def register(details: RegistrationRequest)(implicit ec: ExecutionContext): Future[AgentReference] =
-    repository.create(details)
+  def register(request: RegistrationRequest)(implicit ec: ExecutionContext): Future[Either[Failure, AgentReference]] = {
+    validator.validate(request) match {
+      case Valid(_) => repository.create(request).map(Right(_))
+      case Invalid(failure) => Future.successful(Left(failure))
+
+    }
+
+  }
 
 }
